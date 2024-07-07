48a8ee214a6803ccb1bc20b542612726e0490830
Braden Meyerhoff <156978694+meyerhoff20042@users.noreply.github.com>
2024-07-07 15:55:07 -0500

    Add main family pages via upload
    Adding the four families of my four grandparents

---
+++ main.js
@@ -0,0 +1,35 @@
+function openHome() {
+    window.location.href = "index.html";
+}
+
+function openBio() {
+    window.location.href = "aboutme.html";
+}
+
+function openEggers() {
+    window.location.href = "eggers.html";
+}
+
+function openMeyerhoff() {
+    window.location.href = "meyerhoff.html";
+}
+
+function openMeyerhoffAdolph() {
+    window.location.href = "meyerhoff_adolph.html";
+}
+
+function openMeyerhoffTheodore() {
+    window.location.href = "meyerhoff_theodore.html";
+}
+
+function openCoalson() {
+    window.location.href = "coalson.html";
+}
+
+function openStork() {
+    window.location.href = "stork.html";
+}
+
+function openSullivan() {
+    window.location.href = "sullivan.html";
+}
