44739165d85e31ffb544971c57354b95682a0d49
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-07-20 23:13:01 -0500

    Remove extra "/" from references (makes it work on GitHub Pages)

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 //#region Last Names
 
 function openBiermann() {
-    window.location.href = "/html/families/biermann/biermann.html";
+    window.location.href = "html/families/biermann/biermann.html";
 }
 
 function openCoalson() {
-    window.location.href = "/html/families/coalson/coalson.html";
+    window.location.href = "html/families/coalson/coalson.html";
 }
 
 function openEggers() {
-    window.location.href = "/html/families/eggers/eggers.html";
+    window.location.href = "html/families/eggers/eggers.html";
 }
 
 function openLindenberg() {
-    window.location.href = "/html/families/lindenberg/lindenberg.html";
+    window.location.href = "html/families/lindenberg/lindenberg.html";
 }
 
 //#region Meyerhoff
 
 function openMeyerhoff() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff.html";
 }
 
 function openMeyerhoffAdolph() {
@@ -27,37 +27,37 @@ function openMeyerhoffAdolph() {
 }
 
 function openMeyerhoffAnna() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_anna.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_anna.html";
 }
 
 function openMeyerhoffAncestry() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_ancestry.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_ancestry.html";
 }
 
 function openMeyerhoffClara() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_clara.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_clara.html";
 }
 
 function openMeyerhoffClemens() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_clemens.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_clemens.html";
 }
 
 function openMeyerhoffEmma() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_emma.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_emma.html";
 }
 
 function openMeyerhoffTheodore() {
-    window.location.href = "/html/families/meyerhoff/meyerhoff_theodore.html";
+    window.location.href = "html/families/meyerhoff/meyerhoff_theodore.html";
 }
 
 //#endregion
 
 function openStork() {
-    window.location.href = "/html/families/stork/stork.html";
+    window.location.href = "html/families/stork/stork.html";
 }
 
 function openSullivan() {
-    window.location.href = "/html/families/sullivan/sullivan.html";
+    window.location.href = "html/families/sullivan/sullivan.html";
 }
 
 //#endregion
@@ -73,11 +73,11 @@ function openHome() {
 }
 
 function openList() {
-    window.location.href = "/html/list.html";
+    window.location.href = "html/list.html";
 }
 
 function openProjects() {
-    window.location.href = "/html/other.html";
+    window.location.href = "html/other.html";
 }
 
 //#endregion
@@ -85,11 +85,11 @@ function openProjects() {
 //#region Royalty
 
 function openRoyalty() {
-    window.location.href = "/html/royalty/royalty.html";
+    window.location.href = "html/royalty/royalty.html";
 }
 
 function openRoyaltyBritain() {
-    window.location.href = "/html/royalty/royalty_britain.html";
+    window.location.href = "html/royalty/royalty_britain.html";
 }
 
 //#endregion
