847d779b32596a92ab60ae3e24fa0c3288f9b6ef
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-07-20 20:47:36 -0500

    Move files to separate directories

---
+++ js/main.js
@@ -0,0 +1,79 @@
+function openBiermann() {
+    window.location.href = "/html/families/biermann/biermann.html";
+}
+
+function openBio() {
+    window.location.href = "/html/aboutme.html";
+}
+
+function openBritain() {
+    window.location.href = "/royalty/royalty_britain.html";
+}
+
+function openCoalson() {
+    window.location.href = "/html/families/coalson/coalson.html";
+}
+
+function openEggers() {
+    window.location.href = "/html/families/eggers/eggers.html";
+}
+
+function openHome() {
+    window.location.href = "/html/index.html";
+}
+
+function openLindenberg() {
+    window.location.href = "/html/families/lindenberg/lindenberg.html";
+}
+
+function openList() {
+    window.location.href = "/html/list.html";
+}
+
+function openMeyerhoff() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff.html";
+}
+
+function openMeyerhoffAdolph() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_adolph.html";
+}
+
+function openMeyerhoffAnna() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_anna.html";
+}
+
+function openMeyerhoffAncestry() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_ancestry.html";
+}
+
+function openMeyerhoffClara() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_clara.html";
+}
+
+function openMeyerhoffClemens() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_clemens.html";
+}
+
+function openMeyerhoffEmma() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_emma.html";
+}
+
+function openMeyerhoffTheodore() {
+    window.location.href = "/html/families/meyerhoff/meyerhoff_theodore.html";
+}
+
+function openProjects() {
+    window.location.href = "/html/other.html";
+}
+
+function openRoyalty() {
+    window.location.href = "/html/royalty/royalty.html";
+}
+
+function openStork() {
+    window.location.href = "/html/families/stork/stork.html";
+}
+
+function openSullivan() {
+    window.location.href = "/html/families/sullivan/sullivan.html";
+}
